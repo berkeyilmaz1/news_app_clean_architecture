dd3925a688b00723671e3cedd30d8dbd8cc7c1fc
berke.yz <berkeyilmaz189@gmail.com>
2025-01-11 20:46:39 +0300

    add NewsModel

---
+++ lib/features/news/data/models/news_model.dart
@@ -0,0 +1,25 @@
+import 'package:news_app/features/news/domain/entities/news_entitiy.dart';
+
+class NewsModel extends NewsEntity {
+  const NewsModel({
+    super.author,
+    super.title,
+    super.description,
+    super.url,
+    super.urlToImage,
+    super.publishedAt,
+    super.content,
+  });
+
+  factory NewsModel.fromJson(Map<String, dynamic> json) {
+    return NewsModel(
+      author: json['author'] as String?,
+      title: json['title'] as String?,
+      description: json['description'] as String?,
+      url: json['url'] as String?,
+      urlToImage: json['urlToImage'] as String?,
+      publishedAt: json['publishedAt'] as String?,
+      content: json['content'] as String?,
+    );
+  }
+}
