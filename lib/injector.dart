3a851e3d3143d232791801b9eb3396e243aa492e
berke.yz <berkeyilmaz189@gmail.com>
2025-01-11 21:30:19 +0300

    feat: add dependency injection setup for news services and use cases

---
+++ lib/injector.dart
@@ -0,0 +1,46 @@
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+import 'package:news_app/config/constants/service_paths.dart';
+import 'package:news_app/core/service/news_network_manager.dart';
+import 'package:news_app/features/news/data/datasource/news_service.dart';
+import 'package:news_app/features/news/data/repositories/news_repository_impl.dart';
+import 'package:news_app/features/news/domain/repositories/news_repository.dart';
+import 'package:news_app/features/news/domain/usecases/get_news.dart';
+import 'package:news_app/features/news/presentation/providers/news_notifiers.dart';
+
+final getIt = GetIt.instance;
+
+Future<void> init() async {
+  // Core
+  getIt
+    ..registerLazySingleton<Dio>(() {
+      final dio = Dio();
+      return dio;
+    })
+    ..registerLazySingleton<NewsNetworkManager>(
+      () => NewsNetworkManager(
+        dio: getIt(),
+        baseUrl: ServicePaths.baseUrl,
+      ),
+    )
+
+    // Data Sources
+    ..registerLazySingleton<NewsService>(
+      () => NewsService(newsNetworkManager: getIt()),
+    )
+
+    // Repositories
+    ..registerLazySingleton<NewsRepository>(
+      () => NewsRepositoryImpl(newsService: getIt()),
+    )
+
+    // Use Cases
+    ..registerLazySingleton(
+      () => GetNewsUseCase(newsRepository: getIt()),
+    )
+
+    // Notifiers
+    ..registerFactory(
+      () => NewsNotifier(getIt()),
+    );
+}
