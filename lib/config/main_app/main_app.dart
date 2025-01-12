3ba87419a7ee228fe12f6e678c4f60f48f6a437f
berke.yz <berkeyilmaz189@gmail.com>
2025-01-12 22:37:30 +0300

    feat: implement  app theme management

--- lib/config/main_app/main_app.dart
+++ lib/config/main_app/main_app.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:news_app/config/constants/string_constants.dart';
-import 'package:news_app/config/widgets/widget_sizes.dart';
+import 'package:news_app/config/theme/app_theme.dart';
 import 'package:news_app/features/news/presentation/view/news_view.dart';
 
 final class MainApp extends StatelessWidget {
@@ -11,16 +11,7 @@ final class MainApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: StringConstants.appName,
-      theme: ThemeData(
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.transparent,
-          elevation: 0,
-          iconTheme: IconThemeData(
-            color: Colors.white,
-            shadows: <Shadow>[Shadow(blurRadius: WidgetSizes.spacingNormal)],
-          ),
-        ),
-      ),
+      theme: AppTheme.appThemeData(),
       home: const NewsView(),
     );
   }
---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/config/widgets/widget_sizes.dart';
+
+final class AppTheme {
+  AppTheme._();
+
+  static ThemeData appThemeData() {
+    return ThemeData(
+      appBarTheme: const AppBarTheme(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        centerTitle: true,
+        titleTextStyle: TextStyle(
+          fontSize: WidgetSizes.spacingNormal,
+          fontWeight: FontWeight.bold,
+        ),
+        iconTheme: IconThemeData(
+          color: Colors.white,
+          shadows: <Shadow>[Shadow(blurRadius: WidgetSizes.spacingNormal)],
+        ),
+      ),
+    );
+  }
+}
