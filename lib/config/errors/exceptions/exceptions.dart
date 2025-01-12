948ef90b96ecaec759521b9d49f62de4e26eec3c
berke.yz <berkeyilmaz189@gmail.com>
2025-01-12 14:37:25 +0300

    server exception added

--- lib/config/errors/exceptions/exceptions.dart
+++ lib/config/errors/exceptions/exceptions.dart
@@ -1,2 +1,5 @@
 /// [NullResponseException] is thrown when the response is null.
 final class NullResponseException implements Exception {}
+
+/// [ServerException] is thrown when there is an error in the server.
+final class ServerException implements Exception {}
